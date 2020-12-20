78dbc1b326f18d648fc1d87d51a06a7de5825bf8
Geo <megagog19@gmail.com>
2020-12-20 13:46:45 +0200

    Init

---
+++ front_end/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+//<img src={logo} className="App-logo" alt="logo" />
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Henlo
+    </div>
+  );
+}
+
+export default App;
