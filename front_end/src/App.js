cd8e0a020ee892f005d8d99d8ba1664bdd967754
Geo <megagog19@gmail.com>
2020-12-21 13:12:27 +0200

    Add axios

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,13 +1,29 @@
-//import logo from './logo.svg';
-//<img src={logo} className="App-logo" alt="logo" />
-import './App.css';
+import Axios from 'axios'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      Henlo
-    </div>
-  );
+export default class App extends Component {
+
+
+    state = {
+        films: []
+    }
+
+    componentDidMount = () => {
+        Axios
+            .get("http://localhost:50271/api/films")
+            .then(data => {
+                console.dir(data)
+                this.setState({
+                    films: data.data
+                })
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.films.map(film => <h1 key={film.id}>{film.naming}</h1>)}
+            </div>
+        )
+    }
 }
-
-export default App;
